22fdbb68dd9ce25b3ce84a78db8f87ddb31a2c15
Will Foran <willforan@gmail.com>
2016-06-10 11:40:48 -0400

    init

---
+++ ldapauth.go
@@ -0,0 +1,32 @@
+package ldapauth
+
+import (
+    "fmt"
+    "log"
+    "strings"
+    "gopkg.in/ldap.v2"
+)
+
+func AuthLdap(user string,password string) error{
+   var ldap_server string = "acct.upmchs.net"
+   //var domain      string = "1UPMC-ACCT"
+   var ldap_port   uint16 = 389
+
+   var duser string = strings.Join([]string{"1UPMC-ACCT", user},"\\")
+   //var uname string = "1UPMC-ACCT\\foranw"
+
+   l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", ldap_server, ldap_port))
+   //err = l.Bind( uname,password)
+   err = l.Bind(duser,password)
+   return(err)
+
+}
+func IsAuth(user string, password string) bool {
+  err:=AuthLdap(user,password)
+  if(err != nil ){
+    log.Fatal(err)
+    return false
+  } else {
+   return true
+  }
+}
---
+++ ldapauth_test.go
@@ -0,0 +1,23 @@
+package web
+
+import (
+  "testing"
+  "ldapauth"
+  "gopkg.in/ini.v1"
+)
+
+func TestLDAPAuth(t *testing.T){
+  cfg, err := ini.Load("ldapuser.ini")
+  if err != nil{
+   t.Skip("Need ldapuser.ini file with user= and password=")
+  }
+
+  user,uerr := cfg.Section("").GetKey("user")
+  pass,perr := cfg.Section("").GetKey("password")
+  if perr !=nil || uerr != nil { t.Skip("Need ldapuser.ini file with user= and password=") }
+
+  isauth := ldapauth.IsAuth(user.String(),pass.String())
+  if ! isauth {
+    t.Error("Expeciting auth for foranw, not auth'ed!", isauth)
+  }
+}
